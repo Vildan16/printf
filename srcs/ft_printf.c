627c33bb5791d68b1d90d6778f61c623dae657df
Vildan <vildan.asckarov@gmail.com>
2021-01-06 12:22:35 +0300

    some changes

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -5,6 +5,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>  //remove
+#include <stdlib.h>  //remove
 
 typedef struct		s_params
 {
@@ -14,6 +15,76 @@ typedef struct		s_params
 	int 			precision;
 	char 			type;
 }					t_params;
+
+
+
+size_t	ft_strlen(const char *s)  //LIBFT
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static int	ft_check_minint(int n, char *str)  //LIBFT
+{
+	if (n == -2147483648)
+	{
+		str[0] = '-';
+		str[1] = '2';
+		n = 147483648;
+	}
+	return (n);
+}
+
+static int	ft_nn(int n)  //LIBFT
+{
+	int		nn;
+
+	nn = 0;
+	if (n == -2147483648)
+		return (11);
+	if (n <= 0)
+	{
+		nn++;
+		n = -1 * n;
+	}
+	while (n != 0)
+	{
+		n = n / 10;
+		nn++;
+	}
+	return (nn);
+}
+
+char		*ft_itoa(int n)   //LIBFT
+{
+	int		nn;
+	char	*str;
+
+	nn = ft_nn(n);
+	str = (char *)malloc(sizeof(char) * (nn + 1));
+	if (!str)
+		return (NULL);
+	if (n == 0)
+		str[nn - 1] = '0';
+	str[nn] = '\0';
+	n = ft_check_minint(n, str);
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -1 * n;
+	}
+	while (n != 0)
+	{
+		str[nn - 1] = n % 10 + '0';
+		n = n / 10;
+		nn--;
+	}
+	return (str);
+}
 
 int		ft_atoi(const char *nptr) //LIBFT
 {
@@ -84,7 +155,7 @@ void	ft_get_flags(const char **fmt, int *flag_zero, int *flag_minus)
 	}
 }
 
-int		ft_get_width(const char **fmt, va_list ap)
+int		ft_get_width(const char **fmt, va_list *ap)
 {
 	int		result;
 
@@ -92,37 +163,93 @@ int		ft_get_width(const char **fmt, va_list ap)
 	if (**fmt >= '0' && **fmt <= '9')
 		result = ft_atoi(*fmt);
 	if (**fmt == '*')
-		result = va_arg(ap, int);
+		result = va_arg(*ap, int);
 	while ((**fmt >= '0' && **fmt <= '9') || **fmt == '*')
 		*fmt += 1;
 	return(result);
 }
 
-int		ft_get_precision(const char **fmt, va_list ap)
+int		ft_get_precision(const char **fmt, va_list *ap)
 {
 	int		result;
 
 	result = -1;
 	if (**fmt == '.')
 	{
-		result = 0;
 		*fmt += 1;
 		if (**fmt >= '0' && **fmt <= '9')
 			result = ft_atoi(*fmt);
-		if (**fmt == '*')
-			result = va_arg(ap, int);
-		while ((**fmt >= '0' && **fmt <= '9') || **fmt == '*')
+		else if (**fmt == '*')
+			result = va_arg(*ap, int);
+		else
+			result = -1;
+		while ((**fmt >= '0' && **fmt <= '9') || **fmt == '*' || **fmt == '-')
 			*fmt += 1;
 	}
 	return(result);
 }
 
-void	ft_handle_i(t_params params, va_list ap)
+static void		ft_putnbr(int n)
 {
-	ft_putchar('A');
+	int		i;
+	char	a[15];
+
+	i = 0;
+	if (n == 0)
+		ft_putchar('0');
+	if (n == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -1 * n;
+	}
+	while (n != 0)
+	{
+		a[i] = n % 10 + '0';
+		n = n / 10;
+		i++;
+	}
+	while (--i >= 0)
+		ft_putchar(a[i]);
 }
 
-int		ft_handle(const char **fmt, va_list ap)
+void	ft_handle_i(t_params params, va_list *ap)
+{
+	int len;
+	int arg;
+
+	arg = va_arg(*ap, int);
+	len = ft_strlen(ft_itoa(arg));
+	if (params.precision != -1)
+		params.flag_zero = 0;
+	if (!params.flag_minus && params.width >= len)
+	{
+		while (params.width > len)
+		{
+			if (params.flag_zero)
+				ft_putchar('0');
+			else
+				ft_putchar(' ');
+			len++;
+		}
+	}
+	if (params.precision != -1)
+	{
+		while (params.precision > len)
+		{
+			ft_putchar('0');
+			len++;
+		}
+	}
+	ft_putnbr(arg);
+}
+
+int		ft_handle(const char **fmt, va_list *ap)
 {
 	t_params params;
 
@@ -162,7 +289,7 @@ int		ft_printf(const char *fmt, ...)
 		if (*fmt == '%')
 		{
 			fmt++;
-			if (!ft_handle(&fmt, ap))
+			if (!ft_handle(&fmt, &ap))
 				return(-1);
 		}
 		else
@@ -178,6 +305,12 @@ int		ft_printf(const char *fmt, ...)
 
 int		main(void)
 {
-	ft_printf("d");
+	int a;
+	int b;
+
+	a = 555;
+	b = 111;
+	printf("a = %-7d\n", a);
+	ft_printf("b = %-7d\n", b);
 	return (0);
 }
